aa8cb9bb87cec896bdc0f57e234c89e8cb4fdf1a
Zahra Karandish <zkarandish@gmail.com>
2023-11-23 13:12:17 -0500

    check for nay conflicts for the new enrollment

--- SchedMasterFinalWeb/Controllers/StudentDashboardController.cs
+++ SchedMasterFinalWeb/Controllers/StudentDashboardController.cs
@@ -53,6 +53,27 @@ namespace SchedMasterFinalWeb.Controllers
 
                     if (existingEnrollment == null)
                     {
+                        if (HasTimeConflict(enrollment.GroupId, userId))
+                        {
+                            // Time conflict detected, add an error message.
+                            ModelState.AddModelError("", "The selected class session conflicts with your existing schedule.");
+
+                            // Explicitly load the Group entity related to this enrollment
+                            var group = db.Groups.Find(enrollment.GroupId);
+                            if (group != null)
+                            {
+                                // Now you can use group.CourseCode since group is loaded
+                                ViewBag.GroupId = new SelectList(db.Groups.Where(g => g.CourseCode == group.CourseCode), "GroupId", "GroupId", group.GroupId);
+                            }
+                            else
+                            {
+                                // Handle the case where the group doesn't exist for this GroupId
+                                ModelState.AddModelError("", "The group could not be found for the provided GroupId.");
+                            }
+
+                            return View(enrollment);
+                        }
+
                         db.Enrollments.Add(enrollment);
                         db.SaveChanges();
                         TempData["SuccessMessage"] = "You have successfully enrolled in the course.";
@@ -100,6 +121,45 @@ namespace SchedMasterFinalWeb.Controllers
                 return RedirectToAction("Index", "StudentDashboard");
             }
         }
+        public bool HasTimeConflict(string newGroupId, int userId)
+        {
+            // Fech all class sesions for the new group.
+            var newGroupSessions = db.ClassSessions
+                                     .Where(cs => cs.GroupId == newGroupId)
+                                     .ToList();
+
+            // Fetch all class sessions for groups where the student is already enroolled.
+            var studentEnrollments = db.Enrollments
+                                       .Where(e => e.UserId == userId)
+                                       .Select(e => e.GroupId)
+                                       .ToList();
+
+            var existingGroupSessions = db.ClassSessions
+                                          .Where(cs => studentEnrollments.Contains(cs.GroupId))
+                                          .ToList();
+
+            // Check for ay time overlap between new grup sessons and exsting group sessions.
+            foreach (var newSession in newGroupSessions)
+            {
+                foreach (var existingSession in existingGroupSessions)
+                {
+                    if (newSession.Day.Equals(existingSession.Day, StringComparison.OrdinalIgnoreCase))
+                    {
+                        // Check if the new session's start or end time falls within the existing session's time range.
+                        if ((newSession.StartTime >= existingSession.StartTime && newSession.StartTime < existingSession.EndTime) ||
+                            (newSession.EndTime > existingSession.StartTime && newSession.EndTime <= existingSession.EndTime))
+                        {
+                            // Time conflict found.
+                            return true;
+                        }
+                    }
+                }
+            }
+
+            // No conflict found.
+            return false;
+        }
+
 
 
 
